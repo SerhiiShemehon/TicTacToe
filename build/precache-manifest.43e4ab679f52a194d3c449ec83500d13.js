c2a1aa9bdbe561f4d8732b0830cd9b9dd948561f
SerhiiShemehon <sergei.shemegon.ua@gmail.com>
2020-10-05 18:24:43 +0300

    added build

---
+++ build/precache-manifest.43e4ab679f52a194d3c449ec83500d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dae012825d094688f60a0d5556aecec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc9215f2e67dc7961c57",
+    "url": "/static/css/main.631c4767.chunk.css"
+  },
+  {
+    "revision": "ec7529cc56111b3ee758",
+    "url": "/static/js/2.4370fb5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4370fb5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9215f2e67dc7961c57",
+    "url": "/static/js/main.e5a60fb5.chunk.js"
+  },
+  {
+    "revision": "441cfea73f775386a3f5",
+    "url": "/static/js/runtime-main.02030f09.js"
+  }
+]);
